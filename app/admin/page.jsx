4fb6b4969449c329ebf3c4b5d0ba27070a367839
Sankalp Sharma <sankalpsharma250@gmail.com>
2024-10-06 00:36:06 +0530

    fix

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,4 +1,5 @@
-'use-client'
+'use client'
+
 import { useEffect, useState } from 'react'
 import Navigation from '../../components/Navigation'
 import AdminPoemList from '../../components/AdminPoemList'
